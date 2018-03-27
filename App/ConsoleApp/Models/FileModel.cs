bb352f7b93a0cdbfa06238fbaa293abe60bee8db
localhost://mefistofeles <iblanquel@gmail.com>
2018-03-27 09:36:47 -0600

    step 0.01

---
+++ App/ConsoleApp/Models/FileModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Models
+{
+    public class FileModel
+    {
+        public string d_codigo { get; set; }
+        public string d_asenta { get; set; }
+        public string d_tipo_asenta { get; set; }
+        public string D_mnpio { get; set; }
+        public string d_estado { get; set; }
+        public string d_ciudad { get; set; }
+        public string d_CP { get; set; }
+        public string c_estado { get; set; }
+        public string c_oficina { get; set; }
+        public string c_tipo_asenta { get; set; }
+        public string c_mnpio { get; set; }
+        public string id_asenta_cpcons { get; set; }
+        public string d_zona { get; set; }
+        public string c_cve_ciudad { get; set; }
+    }
+}
