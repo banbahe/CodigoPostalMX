b0b5dbfc7442791e4551b7571d75bd3eec9dcc04
LAPTOP-9HGCL8NC\inmotion <iblanquel@gmail.com>
2018-08-07 20:15:04 -0500

    user validate if exist

--- school/controllers/user.controller.js
+++ school/controllers/user.controller.js
@@ -36,44 +36,58 @@ module.exports = {
         var d = new Date();
         var n = d.getTime();
 
-        let user = userEntity({
-            id_item: 0,
-            status_item: 1,
-            maker: req.body.email,
-            create_date: n,
-            modification_date:n,
-            email: req.body.email,
-            password: req.body.password,
-            typeUser: req.body.typeUser,
-            username: '',
-            name: '',
-            lastname: '',
-            lastname2: '',
-            alternatemail: '',
-            birthday: n,
-            rfc: '',
-            curp: '',
-            genre: 0,
-            zipcode: '',
-            home_reference: '',
-            apartment_number: '',
-            telephone_number: '',
-            telephone_number2: ''
+        // check if exist email
+        let query = userEntity.find({
+            email: req.body.email
         });
-
-        user.save(function (err) {
+        query.exec(function (err, docs) {
             if (err) {
-                return err;
+                console.log('error' + err);
+                responseutil.Send(res, 400, '', false, ('error' + err), '', '');
+            }
+
+            if (docs.length >= 1) {
+                responseutil.Send(res, 400, '', false, 'Usuario ya existe', '', '');
             } else {
-                responseutil.Send(res, 200, JSON.stringify(user), 'OK', '', '');
+                let user = userEntity({
+                    id_item: 0,
+                    status_item: 1,
+                    maker: req.body.email,
+                    create_date: n,
+                    modification_date: n,
+                    email: req.body.email,
+                    password: req.body.password,
+                    typeUser: req.body.typeUser,
+                    username: '',
+                    name: '',
+                    lastname: '',
+                    lastname2: '',
+                    alternatemail: '',
+                    birthday: n,
+                    rfc: '',
+                    curp: '',
+                    genre: 0,
+                    zipcode: '',
+                    home_reference: '',
+                    apartment_number: '',
+                    telephone_number: '',
+                    telephone_number2: ''
+                });
+
+                user.save(function (err) {
+                    if (err) {
+                        return err;
+                    } else {
+                        responseutil.Send(res, 200, JSON.stringify(user), 'OK', '', '');
+                    }
+                });
             }
         });
-
     },
     GetAll: function (req, res) {
         userEntity.find({}, function (err, users) {
             res.status(200).send(users);
-          //  responseutil.Send(res, 200, JSON.stringify(user), 'OK', '', '');
+            //  responseutil.Send(res, 200, JSON.stringify(user), 'OK', '', '');
 
         });
     }
